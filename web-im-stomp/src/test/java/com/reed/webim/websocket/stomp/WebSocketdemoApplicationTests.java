d85078f7573ae2ab54c344212c0f22aebeae543b
reedF <fym_82@163.com>
2019-04-22 18:36:40 +0800

    upgrade to springboot 2.x

--- web-im-stomp/src/test/java/com/reed/webim/websocket/stomp/WebSocketdemoApplicationTests.java
+++ web-im-stomp/src/test/java/com/reed/webim/websocket/stomp/WebSocketdemoApplicationTests.java
@@ -2,12 +2,12 @@ package com.reed.webim.websocket.stomp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = WebSocketdemoApplication.class)
+@SpringBootTest(classes = WebSocketdemoApplication.class)
 @WebAppConfiguration
 public class WebSocketdemoApplicationTests {
 
